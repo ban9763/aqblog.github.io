efc8f050432c26d643431322d29ae548e0b11b49
ban9763 <2248993992@qq.com>
2025-05-27 22:11:37 +0800

    Site updated: 2025-05-27 22:11:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/24/hello-world/"];function toRandomPost(){
+var posts=["2025/05/24/hello-world/","2025/05/24/Docker快速入门/","2025/05/24/Docker部署Nginx/","2024/03/24/Nginx使用指南/","2024/03/24/Docker部署前端项目/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
